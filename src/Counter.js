717f62ba7c5ee8abd79a5193c8156fe27ecf54ca
James Armbrust <james.armbrust@gmail.com>
2017-11-04 10:21:32 -0600

    in progress

--- src/Counter.js
+++ src/Counter.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, {Component } from 'react';
 
 /**
 * A counter button: tap the button to increase the count.
 */
-class Counter extends React.Component {
- constructor() {
-   super();
-   this.state = {
-     count: 0,
-   };
- }
+class Counter extends Component {
+    constructor() {
+        super();
+        this.state = {
+            count: 0,
+        };
 
- render() {
-   return (
-     <button
-       onClick={() => {
-         this.setState({ count: this.state.count + 1 });
-       }}
-     >
-       Count: {this.state.count}
-     </button>
-   );
- }
+        this.increment = this.increment.bind(this);
+        this.decrement = this.decrement.bind(this);
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={ this.increment }>increment</button>
+                <button onClick={ this.decrement }>decrement</button>
+                <div className="counter"> Count {this.state.count} </div>
+            </div>
+        );
+    }
+
+    increment(event) {
+        this.setState({ count: this.state.count + 1 });
+    }
+    
+    decrement(event) {
+        this.setState({ count: this.state.count - 1 });
+    }
 }
+
 export default Counter;
